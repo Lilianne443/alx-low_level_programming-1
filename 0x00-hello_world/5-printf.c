79bbfe7151e94d6b9b1317cd86f3dd609caaf2c0
Echebiri <echebiriemmanueluchechukwu@gmail.com>
2023-02-16 15:41:45 -0800

    C  Hello World

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
-#include<stdio.h>
+#Include <stdio.h>
 /**
  * main - Entry point
- *
- * Return: Always 0 (Success)
+ * Return: 0 (Success)
  */
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
-	return (0);
+printf("with proper grammer, but the outcome is a piece of art,\n")
+return (0);
 }
