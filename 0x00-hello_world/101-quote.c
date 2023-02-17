e450d7c07e17a9a2ea5ba85a5bd8293c1d10a692
Echebiri <echebiriemmanueluchechukwu@gmail.com>
2023-02-16 17:41:50 -0800

    C  Hello World updated

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <studio.h>
+#include <studio.h>
+/**
+ * main - Entry
+ * Return: 1
+ */
+int main(viod)
+{
+	write(1, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59;
+	return (1);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#Include <stdio.h>
+/**
+ * main - A program that prints thesize of various cpmputer types
+ * Return: (Success)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+printf("Size of a char: %i byte(s)\n", (unsigned long)sizeof(a);
+printf("Size of a int: %i byte(s)\n", (unsigned long)sizeof(b);
+printf("Size of a long int: %i byte(s)\n", (unsigned long)sizeof(c);
+printf("Size of a long long int: %i byte(s)\n", (unsigned long)sizeof(d);
+printf("Size of a float: %i byte(s)\n", (unsigned long)sizeof(f);
+return (0);
+}
