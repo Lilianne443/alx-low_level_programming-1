b4902a02c869445ef2d420ec63e5e1d67e836075
Echebiri <echebiriemmanueluchechukwu@gmail.com>
2023-02-18 06:53:55 -0800

    0x01 C - Variables if else while

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,8 @@
-#include <stdio.h>
 #include <stdlib.h>
+
 #include <time.h>
+
+#include <stdio.h>
 
 /**
  * main - print if the number is positive, negative or 0
@@ -15,15 +17,15 @@ int main(void)
 
 	if (n > 0)
 	{
-		printf("%d is positive\n", n);
+	printf("%d is positive\n", n);
 	}
 	else if (n == 0)
 	{
-		printf("%d is zero\n");
+	printf("%d is zero\n", n);
 	}
 	else
 	{
-		printf("%d is negative\n");
+	printf("%d is negative\n", n);
 	}
 
 	return (0);
