7872c86de88e2f4940a8c6691b8ded72fdd82c7a
Echebiri <echebiriemmanueluchechukwu@gmail.com>
2023-02-21 15:38:15 -0800

    0x02 functions_nested_loops

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -15,7 +15,7 @@ void jack_bauer(void)
 	{
 	for (b = 0; b <= 9; b++)
 	{
-	for ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
+	if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
 	{
 	for (c = 5; c <= 5; c++)
 	{
