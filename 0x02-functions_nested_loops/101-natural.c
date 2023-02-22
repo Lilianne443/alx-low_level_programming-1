e5527b688aaf4bd2063b962a1c4b24814e21791a
Echebiri <echebiriemmanueluchechukwu@gmail.com>
2023-02-21 23:21:46 -0800

    0x02 functions_nested_loops

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stclib.h>
 
 /**
  * main - Prints thesum of all multiples of 3 or 5 up to 1024
@@ -9,7 +8,7 @@
 int main(void)
 
 {
-	int i = 0, z = 0;
+	int i, z = 0;
 
 	while (i < 1024)
 	{
