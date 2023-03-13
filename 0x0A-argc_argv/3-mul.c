e9729d06d43afa7029b3e59bd8a7f4c9d9d0cae3
Echebiri <echebiriemmanueluchechukwu@gmail.com>
2023-03-12 23:48:52 -0700

    0x0A-argc_argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,8 +1,5 @@
-
-
 #include <stdio.h>
 #include <stdlib.h>
-
 
 /**
 * main - multiplies two numbers.
@@ -13,20 +10,17 @@
 */
 int main(int argc, char **argv)
 {
-int x, y;
+	int x, y;
 
+	if (argc < 3)
+	{
+	printf("Error\n");
+	return (1);
+	}
 
-if (argc < 3)
-{
-printf("Error\n");
-return (1);
-}
+	x = atoi(argv[1]);
+	y = atoi(argv[2]);
+	printf("%d\n", x * y);
 
-
-x = atoi(argv[1]);
-y = atoi(argv[2]);
-printf("%d\n", x * y);
-
-
-return (0);
+	return (0);
 }
